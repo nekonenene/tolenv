eac72625b0582314a01220b987017d5877c339f1
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-03-20 14:41:04 +0900

    harib00j: io_hlt 関数を naskfunc.nas を使い追加

--- z_tools/bootpack.c
+++ z_tools/bootpack.c
@@ -1,5 +1,8 @@
+// naskfunc.nas で作った関数を使うことをCコンパイラに教える
+void io_hlt(void);
+
 void HariMain(void) {
 fin:
-  /* ここにHLTを入れたいのだが、C言語ではHLTが使えない！ */
+  io_hlt(); // naskfunc.nas の _io_hlt が実行される
   goto fin;
 }
